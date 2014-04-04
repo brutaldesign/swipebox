a5312d550b66cb59f9ea278ee9d442ee0c223412
baranga <henning.panke@gmail.com>
2014-04-04 08:42:07 +0200

    declare index as local var

--- src/js/jquery.swipebox.js
+++ src/js/jquery.swipebox.js
@@ -642,7 +642,7 @@
 			 */
 			getNext : function () {
 				var $this = this;
-				index = $( '#swipebox-slider .slide' ).index( $( '#swipebox-slider .slide.current' ) );
+				var index = $( '#swipebox-slider .slide' ).index( $( '#swipebox-slider .slide.current' ) );
 				if ( index+1 < elements.length ) {
 					index++;
 					$this.setSlide( index );
@@ -661,7 +661,7 @@
 			 * Get previous slide
 			 */
 			getPrev : function () {
-				index = $( '#swipebox-slider .slide' ).index( $( '#swipebox-slider .slide.current' ) );
+				var index = $( '#swipebox-slider .slide' ).index( $( '#swipebox-slider .slide.current' ) );
 				if ( index > 0 ) {
 					index--;
 					this.setSlide( index );
